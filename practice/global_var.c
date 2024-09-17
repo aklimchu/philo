faf28730575de8294b49bee5eb8ba2d9451fcbda
aklimchu <aklimchu@student.hive.fi>
2024-09-17 12:24:37 +0300

    practice tasks added

---
+++ practice/global_var.c
@@ -0,0 +1,34 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	g = 0;
+
+void	*myThreadFun(void *vargp)
+{
+	int	myid = getpid();
+
+	static int s = 0;
+
+	++s;
+	++g;
+
+	printf("Thread ID: %d, Static: %d, Global: %d\n", myid, ++s, ++g);
+	return (NULL);
+}
+
+int main()
+{
+	int i;
+	pthread_t tid;
+
+	for (i = 0; i < 3; i++)
+	{
+		pthread_create(&tid, NULL, myThreadFun, NULL);
+		printf("%lu\n", tid);
+	}
+	
+	pthread_exit(NULL);
+	return (0);
+}
